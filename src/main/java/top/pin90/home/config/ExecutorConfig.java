880126d591bf76e1a0f32713de9dbc3b5a744a1f
Shipin <shpzhao@rxhui.com>
2023-01-05 18:16:11 +0800

    jdk19 虚拟线程支持

--- src/main/java/top/pin90/home/config/ExecutorConfig.java
+++ src/main/java/top/pin90/home/config/ExecutorConfig.java
@@ -1,6 +1,5 @@
 package top.pin90.home.config;
 
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,23 +7,39 @@ import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import static org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME;
 
 @Configuration
 public class ExecutorConfig {
 
-    @Bean(name = { APPLICATION_TASK_EXECUTOR_BEAN_NAME,
-            AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME })
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    @Bean
+    public ThreadFactory virtualThreadFactory() {
+        return Thread
+                .ofVirtual()
+                .name("Virtual Thread-", 0)
+                .factory();
     }
 
     @Bean
-    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+    public ExecutorService virtualExecutorService(ThreadFactory virtualExecutorService) {
+        return Executors.newThreadPerTaskExecutor(virtualExecutorService);
+    }
+
+    @Bean(name = {APPLICATION_TASK_EXECUTOR_BEAN_NAME,
+            AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME})
+    public AsyncTaskExecutor asyncTaskExecutor(ExecutorService virtualExecutorService) {
+        return new TaskExecutorAdapter(virtualExecutorService);
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer(
+            ExecutorService vtExecutorService) {
         return protocolHandler -> {
-            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+            protocolHandler.setExecutor(vtExecutorService);
         };
     }
 }
--- src/main/java/top/pin90/home/controller/TestController.java
+++ src/main/java/top/pin90/home/controller/TestController.java
@@ -4,14 +4,54 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 @Slf4j
 public class TestController {
 
+    private AtomicLong count = new AtomicLong(0);
+
     @GetMapping("/t")
-    public String t(){
+    public String t() {
         Thread thread = Thread.currentThread();
         log.info("呵呵哈哈哈 {}", thread);
         return thread.toString();
+    }
+
+    @GetMapping("/currentTime")
+    public String currentTime() {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long time = System.currentTimeMillis();
+//        Thread thread = Thread.currentThread();
+//        List<Thread> vtList = null;
+//        try {
+//            Field containerField = Thread.class.getDeclaredField("container");
+//            Field carrierThreadField = thread.getClass().getDeclaredField("carrierThread");
+//            containerField.setAccessible(true);
+//            carrierThreadField.setAccessible(true);
+//            ThreadContainer container = (ThreadContainer) containerField.get(thread);
+//            Thread carrierThread = (Thread) carrierThreadField.get(thread);
+//            Method threadStateMethod = Thread.class.getDeclaredMethod("threadState");
+//            threadStateMethod.setAccessible(true);
+////            Thread.State invoke = (Thread.State) threadStateMethod.invoke(carrierThread);
+//            vtList = container.threads().collect(Collectors.toList());
+//
+//            HashMap<Thread.State, Integer> count = new HashMap<>();
+//
+//            for (Thread vt : vtList) {
+//                Thread.State state = (Thread.State) threadStateMethod.invoke(vt);
+//                count.compute(state, (k, v) -> v == null ? 0 : v + 1);
+//            }
+//            log.info("currentTime time {} count {} --- t {}  ------ vtList {}", time, count, thread, vtList);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        log.info("currentTime time {} count {} t {}", time, count.getAndIncrement(), thread);
+        return time + "";
     }
 }
