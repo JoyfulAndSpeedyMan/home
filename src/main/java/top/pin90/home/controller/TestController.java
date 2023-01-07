4c245bc2e49eab3e05bb0a4555830ed5171c6fac
品 <905991211@qq.com>
2023-01-07 10:28:25 +0800

    testHttp

--- src/main/java/top/pin90/home/controller/TestController.java
+++ src/main/java/top/pin90/home/controller/TestController.java
@@ -1,14 +1,19 @@
 package top.pin90.home.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @Slf4j
 public class TestController {
+
+    private RestTemplate restTemplate;
 
     private AtomicLong count = new AtomicLong(0);
 
@@ -29,5 +34,17 @@ public class TestController {
             }
         }
         return System.currentTimeMillis() + "";
+    }
+
+    @GetMapping("/testHttp")
+    public ResponseEntity<String> testHttp(){
+        String uri = "https://www.zhihu.com/market/paid_column/1553436710532493313/section/1553440073525415936?is_share_data=true&vp_share_title=0";
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
+        return forEntity;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 }
